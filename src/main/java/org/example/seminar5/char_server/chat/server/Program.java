323ead1b111322baa4449ebf6733e0eaf826af0c
Artem <Artem@Nesterov.ru>
2024-05-04 13:02:25 +0300

    hw5

---
+++ src/main/java/org/example/seminar5/char_server/chat/server/Program.java
@@ -0,0 +1,21 @@
+package org.example.seminar5.char_server.chat.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+
+public class Program {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1400);
+            Server server = new Server(serverSocket);
+            server.runServer();
+        }
+        catch (UnknownHostException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
